3d75185de347461fe238ca540390e86d3c853e5c
Abhinav Yadav <abhinavy14@gmail.com>
2021-05-29 22:02:03 +0530

    changed version of node-sass in package.json, added cart.utils.js, added addCartItem in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import cartActionTypes from './cart.types';
+import { addCartItem } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addCartItem(state.cartItems, action.payload)
+      }
       default:
         return state;
   }
